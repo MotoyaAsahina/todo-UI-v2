c7bc12e1caf1e37f9bec7f9eea31c72a658c7bb8
MotoyaAsahina <motoyaasahina@icloud.com>
2024-08-01 15:25:14 +0900

    update favicon

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -9,7 +9,10 @@ type MainContentProps = {
 
 export default function MainContent(props: MainContentProps) {
   return (
-    <div className="h-svh w-full md:w-[calc(100vw-200px)] lg:w-[calc(100vw-260px)] bg-slate-200 bg-opacity-60 pt-10">
+    <div
+      className="h-svh w-full md:w-[calc(100vw-200px)] lg:w-[calc(100vw-260px)]
+                 pt-10 <sm:pt-6 bg-slate-200 bg-opacity-60"
+    >
       <div className="w-full h-full flex overflow-scroll px-8 gap-6 <sm:snap-x <sm:snap-mandatory">
         {props.groups.map((group) => (
           <TaskPanel
